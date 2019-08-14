f82b85bffdb425808f9f214d6b5534a9933859bf
LizeLive <github@lize.live>
2019-08-14 13:37:22 -0700

    Add project files.

---
+++ MixplayPc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MixplayPc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = Nefarius.ViGEm.Client.ViGEmClient
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
